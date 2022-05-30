1eec53c32a8533059114d593a1a178054e8ddf2f
tsuzat <aloksingh772002@gmail.com>
2022-05-30 23:49:13 +0530

    Basic Static to do list in flutter

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,124 @@
+import 'package:flutter/material.dart';
+import 'package:to_do_list/utils/to_do_tasks.dart';
+import 'package:to_do_list/widgets/task_list.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final List<Map<String, dynamic>> todoList = ToDoTaskList.tasks;
+  final _formkey = GlobalKey<FormState>();
+  final myController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color.fromRGBO(227, 235, 251, 1),
+      appBar: AppBar(
+        backgroundColor: const Color.fromRGBO(175, 126, 235, 1),
+        leading: const Icon(Icons.menu),
+        centerTitle: true,
+        title: const Text("Todo List"),
+      ),
+      body: Container(
+        margin: const EdgeInsets.all(30),
+        padding: const EdgeInsets.all(10),
+        decoration: const BoxDecoration(color: Colors.white),
+        child: TaskList(
+          todoList: todoList,
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        tooltip: "Add a task in list",
+        backgroundColor: const Color.fromRGBO(175, 126, 235, 1),
+        onPressed: () {
+          showDialog(
+            context: context,
+            builder: (context) => AlertDialog(
+              title: const Center(child: Text("Add a Task")),
+              titleTextStyle: const TextStyle(
+                color: Color.fromRGBO(151, 153, 167, 1),
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+              ),
+              content: SizedBox(
+                height: 150,
+                child: Form(
+                  key: _formkey,
+                  child: Column(
+                    children: <Widget>[
+                      TextFormField(
+                        style: const TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.w500,
+                          color: Color.fromRGBO(151, 153, 167, 1),
+                        ),
+                        cursorColor: const Color.fromRGBO(175, 126, 235, 1),
+                        cursorHeight: 26,
+                        controller: myController,
+                        cursorWidth: 2,
+                        decoration: const InputDecoration(
+                          focusedBorder: UnderlineInputBorder(
+                            borderSide: BorderSide(
+                                width: 2,
+                                color: Color.fromRGBO(175, 126, 235, 1)),
+                          ),
+                          hintText: "your task",
+                          hintStyle: TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.w500,
+                            color: Color.fromRGBO(151, 153, 167, 1),
+                          ),
+                        ),
+                        onChanged: (value) {},
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return "Don't be lazy!!";
+                          }
+                          return null;
+                        },
+                      ),
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      ElevatedButton(
+                        style: TextButton.styleFrom(
+                          backgroundColor:
+                              const Color.fromRGBO(175, 126, 235, 1),
+                          fixedSize: const Size(40, 40),
+                        ),
+                        onPressed: () {
+                          if (_formkey.currentState!.validate()) {
+                            Navigator.of(context).pop();
+                            setState(() {
+                              todoList.add({
+                                'name': myController.text,
+                                'done': false,
+                              });
+                              myController.text = "";
+                            });
+                          }
+                        },
+                        child: const Text("Add"),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          );
+        },
+        child: const Icon(
+          Icons.add,
+          size: 30,
+          color: Colors.white,
+        ),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+    );
+  }
+}
---
+++ lib/widgets/task_list.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class TaskList extends StatefulWidget {
+  final List<Map<String,dynamic>> todoList;
+  const TaskList({
+    Key? key,
+    required this.todoList,
+  }) : super(key: key);
+
+  @override
+  State<TaskList> createState() => _TaskListState();
+}
+
+class _TaskListState extends State<TaskList> {
+  
+  void _deleteItem(int index) {
+    widget.todoList.removeAt(index);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: widget.todoList.length,
+      itemBuilder: (context, index) => ListTile(
+        leading: IconButton(
+          onPressed: () {
+            setState(() {
+              widget.todoList[index]['done'] = !widget.todoList[index]['done'];
+            });
+          },
+          icon: widget.todoList[index]['done']
+              ? const Icon(Icons.circle)
+              : const Icon(Icons.circle_outlined),
+          color: widget.todoList[index]['done']
+              ? const Color.fromRGBO(175, 126, 235, 1)
+              : const Color.fromRGBO(151, 153, 167, 1),
+          iconSize: 26,
+        ),
+        title: Text(
+          widget.todoList[index]['name'],
+          style: TextStyle(
+            fontSize: 16,
+            fontWeight: FontWeight.w500,
+            color: const Color.fromRGBO(151, 153, 167, 1),
+            decorationStyle: TextDecorationStyle.solid,
+            decorationThickness: 3,
+            decorationColor: const Color.fromRGBO(151, 153, 167, 1),
+            decoration:
+                widget.todoList[index]['done'] ? TextDecoration.lineThrough : null,
+          ),
+          overflow: TextOverflow.ellipsis,
+        ),
+        trailing: widget.todoList[index]['done']
+            ? IconButton(
+                onPressed: () {
+                  setState(() {
+                    _deleteItem(index);
+                  });
+                },
+                icon: const Icon(
+                  CupertinoIcons.delete_simple,
+                  size: 22,
+                ),
+              )
+            : null,
+      ),
+    );
+  }
+}
